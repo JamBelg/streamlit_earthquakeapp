0d5949d93134844c4885afc4f7da5bc31c0bc380
Jamel Belgacem <95283203+JamBelg@users.noreply.github.com>
2024-08-15 21:19:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,70 @@
 import streamlit as st
-import folium
-from streamlit_folium import st_folium
 import pandas as pd
 import numpy as np
 import pydeck as pdk
 import boto3
-import requests
-from io import StringIO
-from bokeh.plotting import figure, show
+#from bokeh.plotting import figure, show
+import folium
+from streamlit_folium import st_folium
 
-
+# Function to read data from S3
 @st.cache_data
 def read_data():
-    data = pd.read_csv('C:/users/ch1jbelgac1/Work Folders/MyDocuments/GitHub/streamlit_earthquakeapp/data_offline.csv')
-    data.rename(columns={'Latitude': 'latitude', 'Longitude': 'longitude'}, inplace=True)
+    st.session_state["aws_access_key_id"] = st.secrets["aws_access_key_id"]
+    st.session_state["aws_secret_access_key"] = st.secrets["aws_secret_access_key"]
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=st.session_state["aws_access_key_id"],
+        aws_secret_access_key=st.session_state["aws_secret_access_key"],
+    )
+    bucket_name = "earthquakedb"
+    s3_file = "data_etl.csv"
+    obj = s3.get_object(Bucket=bucket_name, Key=s3_file)
+    data = pd.read_csv(obj["Body"])
 
-    data = data[['Date UTC','Location','Magnitude','latitude','longitude']]
+    data['datetime_str'] = data['Year'].astype(str) + ' ' + data['UTC_Time']
+    data['Date UTC'] = pd.to_datetime(data['datetime_str'], format='%Y %b %d %H:%M:%S')
+    data.rename(columns={'Latitude': 'latitude', 'Longitude': 'longitude'}, inplace=True)
+    data = data[(data["Magnitude"] > 0) & 
+                (data['Date UTC'] >= '2018-01-01')][["Date UTC", "Location", "Magnitude", "Depth", "latitude", "longitude"]]
     return data
 
-@st.cache_data
-def load_csv_from_github(url):
-    try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
-        csv_data = response.content.decode('utf-8')
-        df = pd.read_csv(pd.compat.StringIO(csv_data))
-        return df
-    except requests.exceptions.HTTPError as http_err:
-        st.error(f"HTTP error occurred: {http_err}")
-    except requests.exceptions.ConnectionError as conn_err:
-        st.error(f"Connection error occurred: {conn_err}")
-    except requests.exceptions.Timeout as timeout_err:
-        st.error(f"Timeout error occurred: {timeout_err}")
-    except requests.exceptions.RequestException as req_err:
-        st.error(f"An error occurred: {req_err}")
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
-    return None
+
 
 
 # Streamlit app layout
 st.set_page_config(
-    page_title="Recorded Earthquakes in Switzerland",
+    page_title="Earthquakes in Switzerland",
     page_icon=":earth_africa:",
 )
 
-# Header
+st.sidebar.title("About")
+
+
+st.sidebar.markdown(
+    """
+    This application provides detailed analysis and visualization of recorded earthquakes in Switzerland.
+    Discover insights into seismic activity and track historical earthquake data with interactive charts and maps.
+    
+    Data are from [Swiss Seismological Service](http://seismo.ethz.ch/en/home/).
+    
+    **Developped by Jamel Belgacem**
+    
+    Connect with me:
+
+    <a href="https://www.linkedin.com/in/jamel-belgacem-289606a7/" target="_blank">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" width="30" height="30" alt="LinkedIn"/>
+    </a>
+    
+    <a href="https://github.com/JamBelg" target="_blank">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" width="30" height="30" alt="GitHub"/>
+    </a>
+    """,
+    unsafe_allow_html=True
+)
+
+
+# CSS
 st.markdown(
     """
     <style>
@@ -54,29 +73,31 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-st.title("Recorded Earthquakes in Switzerland")
+
+col1, col2, col3 = st.columns([1, 8, 1])
+with col1:
+    st.image("earthquake(1).png", width=60)
+with col2:
+    st.title("Earthquakes in Switzerland")
 
 # Data fetching and table
 data_load_state = st.text('Loading data...')
-data = load_csv_from_github('https://raw.githubusercontent.com/JamBelg/streamlit_earthquakeapp/main/filename.csv')
-if data is not None:
-    data_load_state.text("Done!")
-else:
-    st.warning("Failed to load the CSV file.")
+data = read_data()
+data_load_state.text("Done!")
 
+st.subheader("Filter data:")
 min_mag, max_mag = st.slider(label='Select Magnitude Range',
                              min_value=0.0,
                              max_value=8.0,
-                             value=(3.0, 5.0),
+                             value=(1.0, 5.0),
                              step=0.5)
 filtered_data = data[(data['Magnitude']>=min_mag) & (data['Magnitude']<=max_mag)]
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(filtered_data.head(20))
-    st.markdown("h5(red[Data from Swiss Seismological Service (http://seismo.ethz.ch/en/home/)])")
 
-st.subheader('Magnitude distribution')
+st.subheader("Earthquake's magnitude distribution")
 # Calculate histogram values
 hist_values, bin_edges = np.histogram(filtered_data['Magnitude'], bins=10, range=(min_mag, max_mag))
 
@@ -105,10 +126,10 @@ m = folium.Map(location=[filtered_data['latitude'].mean(),
 m.fit_bounds(bounds)
 
 # Function to scale the radius
-#def scale_radius(magnitude):
-#    return 2 ** magnitude  # Exponential scaling
 def scale_radius(magnitude):
-    return np.log1p(magnitude) * 10  # Logarithmic scaling, multiplied to enhance size
+    return 2 ** (magnitude)  # Exponential scaling
+#def scale_radius(magnitude):
+#    return np.log1p(magnitude) * 20  # Logarithmic scaling, multiplied to enhance size
 
 # Add points to the map
 for i, row in filtered_data.iterrows():
@@ -121,30 +142,17 @@ for i, row in filtered_data.iterrows():
         popup=f"Magnitude: {row['Magnitude']}"
     ).add_to(m)
 
-#col1, col2 = st.columns([8, 5])
-
 
 # Display the map in Streamlit
-#with col1:
 st_folium(m, width=725)
 
 st.subheader('Frequence of recorded earthquakes')
-# Count the occurrences for each date
-date_counts = filtered_data['Date UTC'].value_counts().sort_index()
+# Group by year and month and count the occurrences
+filtered_data['Year-Month'] = filtered_data['Date UTC'].dt.to_period('M')
+month_counts = filtered_data['Year-Month'].value_counts().sort_index()
 
-# Create a DataFrame from the counts
-date_counts_df = pd.DataFrame({'Date': date_counts.index, 'Count': date_counts.values})
+# Convert the result to a DataFrame
+month_counts_df = pd.DataFrame({'Month': month_counts.index.astype(str), 'Count': month_counts.values})
 
 # Plot the counts using st.bar_chart
-#with col2:
-st.bar_chart(date_counts_df.set_index('Date'))
-
-
-# Footer
-st.markdown(
-    """---
-    Author: Jamel Belgacem  
-    This dashboard was developed with Streamlite.
-    """,
-    unsafe_allow_html=True,
-)
+st.bar_chart(month_counts_df.set_index('Month'))
